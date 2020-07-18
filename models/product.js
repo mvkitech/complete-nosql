d8256b2cd432592949cdbf3329af3e42ee42cf02
mvkitech <info@mvkitech.com>
2020-07-18 16:54:54 -0600

    Check in just prior to swapping over to mongoDB

---
+++ models/product.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../util/database');
+
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: Sequelize.STRING,
+  description: {
+    type: Sequelize.STRING(1023),
+    allowNull: false,
+  },
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Product;
